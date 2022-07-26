492ac5563c80ae6fc9066740ee012ce239e5192e
iamgm <iamgm@yandex.ru>
2022-07-26 08:45:39 +0300

    done

---
+++ type_os.py
@@ -0,0 +1,21 @@
+TYPE_OS = 1 # 1 - Windows; 2 - Linux
+
+class DialogWindows:
+    name_class = "DialogWindows"
+
+
+class DialogLinux:
+    name_class = "DialogLinux"
+
+
+class Dialog:
+    def __new__(cls, *args, **kwargs):
+        obj = None
+        if TYPE_OS == 1:
+            obj = super().__new__(DialogWindows)
+        else:
+            obj = super().__new__(DialogLinux)
+        obj.name = args[0]
+        return obj
+
+dlg = Dialog('new')
