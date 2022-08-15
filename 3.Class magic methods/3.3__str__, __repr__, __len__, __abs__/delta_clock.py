9b136443172d5cd0afeb05689d9278d1303ddce6
iamgm <iamgm@yandex.ru>
2022-08-15 23:45:57 +0300

    __str__ __len__ methods example

---
+++ 3.Class magic methods/3.3__str__, __repr__, __len__, __abs__/delta_clock.py
@@ -0,0 +1,65 @@
+import time
+
+class Clock:
+    def __init__(self, hours, minutes, seconds):
+        self.hours   = hours
+        self.minutes = minutes
+        self.seconds = seconds
+    
+    def get_time(self):
+         return self.hours * 3600 + self.minutes * 60 + self.seconds
+
+class DeltaClock:
+    def __init__(self, clock1, clock2):
+        self.clock1 = clock1
+        self.clock2 = clock2
+    
+    def __len__(self):
+        delta = self.clock1.get_time() - self.clock2.get_time()
+        return delta if delta>0 else 0
+
+    def __str__(self):
+        return time.strftime("%H: %M: %S", time.gmtime(len(self)))
+
+# print(time.ctime(10))
+# print(time.strftime('%H: %M: %S', time.gmtime(70)))
+# # print(time.gmtime(10))
+# struct = time.localtime()
+# print(struct)
+
+
+dt = DeltaClock(Clock(2, 45, 0), Clock(1, 15, 0))
+print(dt) # 01: 30: 00
+len_dt = len(dt) # 5400
+print(len_dt)
+# time.strftime('%H: %M: %S', time.gmtime(разница времен clock1 - clock2))
+
+# Подвиг 8. Объявите класс DeltaClock для вычисления разницы времен. Объекты этого класса должны создаваться командой:
+
+# dt = DeltaClock(clock1, clock2)
+# где clock1, clock2 - объекты другого класса Clock для хранения текущего времени. Эти объекты должны создаваться командой:
+
+# clock = Clock(hours, minutes, seconds)
+# где hours, minutes, seconds - часы, минуты, секунды (целые неотрицательные числа).
+
+# В классе Clock также должен быть (по крайней мере) один метод (возможны и другие):
+
+# get_time() - возвращает текущее время в секундах (то есть, значение hours * 3600 + minutes * 60 + seconds).
+
+# После создания объекта dt класса DeltaClock, с ним должны выполняться команды:
+
+# str_dt = str(dt)   # возвращает строку разницы времен clock1 - clock2 в формате: часы: минуты: секунды
+# len_dt = len(dt)   # разницу времен clock1 - clock2 в секундах (целое число)
+# print(dt)   # отображает строку разницы времен clock1 - clock2 в формате: часы: минуты: секунды
+# Если разность получается отрицательной, то разницу времен считать нулевой.
+
+# Пример использования классов (эти строчки в программе писать не нужно):
+
+# dt = DeltaClock(Clock(2, 45, 0), Clock(1, 15, 0))
+# print(dt) # 01: 30: 00
+# len_dt = len(dt) # 5400
+# Обратите внимание, добавляется незначащий ноль, если число меньше 10.
+
+# P.S. На экран ничего выводить не нужно, только объявить классы.
+
+# Напишите программу. Тестируется через stdin → stdout
