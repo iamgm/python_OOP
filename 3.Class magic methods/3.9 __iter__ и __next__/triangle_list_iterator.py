cf095ded865f6a253e7d52c2d57718e06dc717c0
iamgm <iamgm@yandex.ru>
2022-09-04 22:52:04 +0300

    __iter__ __next__

--- 3.Class magic methods/3.9 __iter__ и __next__/triangle_list_iterator.py
+++ 3.Class magic methods/3.9 __iter__ и __next__/triangle_list_iterator.py
@@ -14,19 +14,33 @@ class TriangleListIterator:
         return self.lst[r][c]
 
 
-l = [[y for y in range(x+1)] for x in range(10)]
-# l = lst = [['x00', 'x01', 'x02'],
-#        ['x10', 'x11'],
-#        ['x20', 'x21', 'x22', 'x23', 'x24'],
-#        ['x30', 'x31', 'x32', 'x33']]
-[print(x) for x in l]
-it = TriangleListIterator(l)
-for x in it:
-    print(x)
+# l = [[y for y in range(x+1)] for x in range(10)]
+# [print(x) for x in l]
 
-it_iter = iter(it)
-# x = next(it_iter)
+# it = TriangleListIterator(l)
+# it_iter = iter(it)
+# [([print(next(it), end = " ") for _ in range(x+1)], print()) for x in range(10)]
 
-[([print(next(it), end = " ") for _ in range(x+1)], print()) for x in range(10)]
+# Подвиг 6. Вам дают задание разработать итератор для последовательного перебора элементов вложенных (двумерных) списков следующей структуры:
+
+# lst = [[x00],
+#        [x10, x11],
+#        [x20, x21, x22],
+#        [x30, x31, x32, x33],
+#        ...
+#       ]
+# Для этого необходимо в программе объявить класс с именем TriangleListIterator, объекты которого создаются командой:
+
+# it = TriangleListIterator(lst)
+# где lst - ссылка на перебираемый список.
+
+# Затем, с объектами класса TriangleListIterator должны быть доступны следующие операции:
+
+# for x in it:  # последовательный перебор всех элементов списка: x00, x10, x11, x20, ...
+#     print(x)
+
 # it_iter = iter(it)
 # x = next(it_iter)
+# Итератор должен перебирать элементы списка по указанной треугольной форме. Даже если итератору на вход будет передан прямоугольная таблица (вложенный список), то ее перебор все равно должен осуществляться по треугольнику. Если же это невозможно (из-за структуры списка), то естественным образом должна возникать ошибка IndexError: index out of range (выход индекса за допустимый диапазон).
+
+# P.S. В программе нужно объявить только класс. Выводить на экран ничего не нужно.
