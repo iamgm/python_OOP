5e7ae67f1e042a219a76ad80d64e596bedbc4485
iamgm <iamgm@yandex.ru>
2022-09-07 02:06:06 +0300

    magic methods

--- 3.Class magic methods/3.10 Magic methods/tic_tac_toe.py
+++ 3.Class magic methods/3.10 Magic methods/tic_tac_toe.py
@@ -9,27 +9,31 @@ class TicTacToe:
         self.pole = tuple(tuple(Cell() for x in range(self.N)) \
                     for _ in range(self.N))
         self.init()
+        self.lines= ((0,0), (0,1), (0,2)), ((1,0), (1,1), (1,2)),\
+                    ((2,0), (2,1), (2,2)), ((0,0), (1,0), (2,0)),\
+                    ((0,1), (1,1), (2,1)), ((0,2), (1,2), (2,2)),\
+                    ((0,0), (1,1), (2,2)), ((0,2), (1,1), (2,0))
 
     def init(self):
         for row in self.pole:
             for c in row:
                 c.value = self.FREE_CELL
-        self.is_human_win = self.is_computer_win = self.is_draw = False
+        self.__is_human_win = self.__is_computer_win = self.__is_draw = False
 
     def show(self):
         [([print(c.value, end=" ") for c in r], print()) for r in self.pole]
 
     def human_go(self):
         r, c = random.choice(self.free_cells())
-        self.pole[r][c].value = self.HUMAN_X
+        self[r, c] = self.HUMAN_X
 
     def computer_go(self):
         r, c = random.choice(self.free_cells())
-        self.pole[r][c].value = self.COMPUTER_O
+        self[r, c] = self.COMPUTER_O
 
     def free_cells(self):
         return [(i,j) for i in range(self.N) for j in range(self.N) \
-            if self.pole[i][j].value == self.FREE_CELL]
+            if self[i, j] == self.FREE_CELL]
         
     def __getitem__(self, idx):
         r, c = idx
@@ -40,58 +44,48 @@ class TicTacToe:
         r, c = idx
         self.validate_idx(idx)
         self.pole[r][c].value = value
+        bool(self)
 
-    @staticmethod
-    def validate_idx(idx):
+    def validate_idx(self, idx):
         if  not (isinstance(idx[0], (int, float)) and \
             isinstance(idx[1], (int, float)) and \
             (0 <= idx[0] < self.N) and (0 <= idx[1] < self.N)):
             raise IndexError('некорректно указанные индексы')
 
     def __bool__(self):
-        for i in range(self.N):
-            for j in range(self.N)
+        cnt = 0
+        has_free_cells = len(self.free_cells())>0
+        for l in self.lines:
+            score = self.win(l)
+            if score:
+                self.__is_computer_win = score==6
+                self.__is_human_win = score==3
+                return False
+            if not score and has_free_cells:
+                cnt += 1
+        self.__is_draw = cnt == 8 and not has_free_cells
+        return False if cnt < 8 else True
 
-
-        return len(self.free_cells())>0
-
-    def variants(self):
-        p = self.pole
-        l = [
-                p[0], p[1], p[2],  
-                
-                (p[0][0], p[1][0], p[2][0]),
-                (p[0][1], p[1][1], p[2][1]),
-                (p[0][2], p[1][2], p[2][2]),
-                
-                (p[0][0], p[1][1], p[2][2]),
-                (p[0][2], p[1][1], p[2][0])
-            ]
+    def win(self, line):
+        score = 0
+        for el in line:
+            if self[el] == 0:
+                return False
+            score += self[el]
+        return score if (score == 3) or (score == 6) else False 
 
     @property
     def is_human_win(self):
         return self.__is_human_win
     
-    @is_human_win.setter
-    def is_human_win(self, value):
-        self.__is_human_win = value
-    
     @property
     def is_computer_win(self):
         return self.__is_computer_win
-    
-    @is_computer_win.setter
-    def is_computer_win(self, value):
-        self.__is_computer_win = value
     
     @property
     def is_draw(self):
         return self.__is_draw
     
-    @is_draw.setter
-    def is_draw(self, value):
-        self.__is_draw = value
-
 class Cell:
     def __init__(self):
         self.value = 0
@@ -112,17 +106,13 @@ while game:
         game.human_go()
     else:
         game.computer_go()
-
     step_game += 1
     print()
-    
-
-
 game.show()
 
-# if game.is_human_win:
-#     print("Поздравляем! Вы победили!")
-# elif game.is_computer_win:
-#     print("Все получится, со временем")
-# else:
-#     print("Ничья.")
+if game.is_human_win:
+    print("Поздравляем! Вы победили!")
+elif game.is_computer_win:
+    print("Все получится, со временем")
+else:
+    print("Ничья.")
