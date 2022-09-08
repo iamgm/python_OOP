30a858fbb767e7454106eec51a19a2fec8661303
iamgm <iamgm@yandex.ru>
2022-09-09 01:57:36 +0300

    func decorator to decorate class methods

--- 4.Inheritance and polymorphism/4.3 Inheritance. super() function and delegation/integer_params.py
+++ 4.Inheritance and polymorphism/4.3 Inheritance. super() function and delegation/integer_params.py
@@ -30,7 +30,31 @@ class Vector:
         self.__coords = c if not reverse else c[::-1]
 
 
+# Подвиг 7. В программе объявлена функция integer_params для класса Vector, которая применяет к каждому методу класса декоратор integer_params_decorated:
+# def integer_params(cls):
+#     methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+#     for k, v in methods.items():
+#         setattr(cls, k, integer_params_decorated(v))
+#             return cls            
+# @integer_params
+# class Vector:
+#     def __init__(self, *args):
+#         self.__coords = list(args)
 
-vector = Vector(1, 2, 3, 4, 5)
-print(vector[1])
-vector[1] = 20.4 # TypeError
+#     def __getitem__(self, item):
+#         return self.__coords[item]
+
+#     def __setitem__(self, key, value):
+#         self.__coords[key] = value
+
+#     def set_coords(self, *coords, reverse=False):
+#         c = list(coords)
+#         self.__coords = c if not reverse else c[::-1]
+# Декоратор integer_params_decorated должен проверять, чтобы все передаваемые аргументы в методы класса (кроме первого self) были целыми числами (имели тип int). Если это не так, то должно генерироваться исключение командой:
+# raise TypeError("аргументы должны быть целыми числами")
+# Ваша задача объявить эту функцию-декоратор.
+# Пример использования класса (эти строчки в программе не писать):
+# vector = Vector(1, 2)
+# print(vector[1])
+# vector[1] = 20.4 # TypeError
+# P.S. В программе нужно объявить только класс. На экран выводить ничего не нужно.
